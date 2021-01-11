4d8416cae9844afddf9d2a44a253a7cac6a2464c
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-01-11 14:16:23 -0300

    Added the rb file

---
+++ enumerables.rb
@@ -0,0 +1,38 @@
+module Enumerable
+  def my_each
+    # code here
+  end
+
+  def my_each_with_index
+    # code here
+  end
+
+  def my_select
+    # code here
+  end
+
+  def my_all?
+    # code here
+  end
+  
+  def my_any?
+    # code here
+  end
+
+  def my_none?
+    # code here
+  end
+
+  def my_count
+    # code here
+  end
+
+  def my_map
+    # code here
+  end
+
+  def my_inject
+    # code here
+  end
+  
+end
