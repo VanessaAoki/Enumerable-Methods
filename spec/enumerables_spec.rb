0345945369b2c9434981c61bedd35d7be5807033
FdI96 <federicolopezce@gmail.com>
2021-02-22 14:30:54 -0300

    Add 3 more test for my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -13,5 +13,19 @@ describe Enumerable do
     end
   end
 
+  describe "#my_each_with_index" do
+    it "if no block is given, returns an enumerator" do
+      expect(array.my_each_with_index) == array.to_enum
+    end
+
+    it "return all the elements in the array" do
+      expect(array.my_each_with_index).to include "test"
+    end
+
+    it "return all the elements with index" do
+      expect(array.my_each_with_index {|elem, index| puts elem, index}).to eql(%w[ruby test rspec])
+    end
+  end
+
 
 end
