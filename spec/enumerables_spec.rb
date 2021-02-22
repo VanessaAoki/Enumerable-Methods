922fe2cc913ac40f6ed6c9fb2afce066601a1ed9
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-02-22 14:47:41 -0300

    Add the my_all? test

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../enumerables.rb'
 
-array = ["ruby", "test", "rspec"]
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-
 describe Enumerable do
+  let(:array) {["ruby", "test", "rspec"]}
+  let(:numbers) {[1, 2, 3, 4, 5, 6, 7, 8]}
+
   describe "#my_each" do
     it "return all the elements in the array" do
       expect(array.my_each).to include "ruby"
@@ -34,4 +34,25 @@ describe Enumerable do
     end
   end
 
+  describe "#my_all?" do
+    it "returns true if no block is given and none of the elements is nil" do
+      expect(array.my_all?).to eql(true)
+    end
+
+    it "returns true if the block never returns false or nil" do
+      expect(array.my_all? { |word| word.length >= 4 }).to eql(true)
+    end
+
+    it "returns false if the block one or more elements return false" do
+      expect(array.my_all? { |word| word.length >= 5 }).to eql(false)
+    end
+
+    it "returns true if all elements are the same data type" do      
+      expect(array.my_all?(String)).to eql(true)
+    end
+
+    it "returns true if the array is empty" do
+      expect([].my_all?).to eql(true)
+    end
+  end
 end
