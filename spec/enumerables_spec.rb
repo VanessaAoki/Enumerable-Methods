176b126f662bdf1fb3e6f28831effa918c654298
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-02-22 14:35:59 -0300

    Add the my_select test

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../enumerables.rb'
 
 array = ["ruby", "test", "rspec"]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
 describe Enumerable do
   describe "#my_each" do
@@ -18,14 +19,19 @@ describe Enumerable do
       expect(array.my_each_with_index) == array.to_enum
     end
 
-    it "return all the elements in the array" do
-      expect(array.my_each_with_index).to include "test"
-    end
-
     it "return all the elements with index" do
       expect(array.my_each_with_index {|elem, index| puts elem, index}).to eql(%w[ruby test rspec])
     end
   end
 
+  describe "#my_select" do
+    it "if no block is given, returns an enumerator" do
+      expect(array.my_select) == array.to_enum
+    end
+
+    it "return an array with all the elements that match the yield" do
+      expect(numbers.my_select {|num| num.even?}).to eql([2, 4, 6, 8])
+    end
+  end
 
 end
